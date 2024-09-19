f233d54d206057bc5d63cd03227fa164bef9c371
SharoonE <173120176+SharoonEndo@users.noreply.github.com>
2024-09-18 21:51:17 -0500

    project finishd

---
+++ src/components/shared/Footer.jsx
@@ -0,0 +1,12 @@
+import './styles/Footer.css';
+
+const Footer = () => {
+	return (
+		<footer className="footer ">
+			<img className="footer__img" src="/Icono Logo.png" alt="logo" />
+			<p className="footer__text">© Todos los derechos reservados</p>
+		</footer>
+	);
+};
+
+export default Footer;
